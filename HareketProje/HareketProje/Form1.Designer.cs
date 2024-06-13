14f944f0a31918d8b67a4b71affa72205e7b413b
berkiskitoglu <167026233+berkiskitoglu@users.noreply.github.com>
2024-06-13 21:12:43 +0300

    Iliskili Veri Tabani Hareket Tablo Ve Sorgulama Uygulamasi
    bir iş sürecinde veya sistemin bir parçası olarak gerçekleşen işlemleri kaydetmek ve bu işlemleri sorgulamak için kullanılmak için yapıldı

---
+++ HareketProje/HareketProje/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace HareketProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hareketDataSet = new HareketProje.HareketDataSet();
+            this.projeHareketBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.projeHareketTableAdapter = new HareketProje.HareketDataSetTableAdapters.ProjeHareketTableAdapter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.hareketDataSet1 = new HareketProje.HareketDataSet1();
+            this.projeHareketBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.projeHareketTableAdapter1 = new HareketProje.HareketDataSet1TableAdapters.ProjeHareketTableAdapter();
+            this.ürünAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.müşteriDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fiyatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CmbUrun = new System.Windows.Forms.ComboBox();
+            this.CmbMusteri = new System.Windows.Forms.ComboBox();
+            this.CmbPersonel = new System.Windows.Forms.ComboBox();
+            this.TxtFiyat = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.hareketDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projeHareketBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hareketDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projeHareketBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hareketDataSet
+            // 
+            this.hareketDataSet.DataSetName = "HareketDataSet";
+            this.hareketDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // projeHareketBindingSource
+            // 
+            this.projeHareketBindingSource.DataMember = "ProjeHareket";
+            this.projeHareketBindingSource.DataSource = this.hareketDataSet;
+            // 
+            // projeHareketTableAdapter
+            // 
+            this.projeHareketTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ürünAdDataGridViewTextBoxColumn,
+            this.müşteriDataGridViewTextBoxColumn,
+            this.personelDataGridViewTextBoxColumn,
+            this.fiyatDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.projeHareketBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1300, 290);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // hareketDataSet1
+            // 
+            this.hareketDataSet1.DataSetName = "HareketDataSet1";
+            this.hareketDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // projeHareketBindingSource1
+            // 
+            this.projeHareketBindingSource1.DataMember = "ProjeHareket";
+            this.projeHareketBindingSource1.DataSource = this.hareketDataSet1;
+            // 
+            // projeHareketTableAdapter1
+            // 
+            this.projeHareketTableAdapter1.ClearBeforeFill = true;
+            // 
+            // ürünAdDataGridViewTextBoxColumn
+            // 
+            this.ürünAdDataGridViewTextBoxColumn.DataPropertyName = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.HeaderText = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.Name = "ürünAdDataGridViewTextBoxColumn";
+            // 
+            // müşteriDataGridViewTextBoxColumn
+            // 
+            this.müşteriDataGridViewTextBoxColumn.DataPropertyName = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.HeaderText = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.Name = "müşteriDataGridViewTextBoxColumn";
+            // 
+            // personelDataGridViewTextBoxColumn
+            // 
+            this.personelDataGridViewTextBoxColumn.DataPropertyName = "Personel";
+            this.personelDataGridViewTextBoxColumn.HeaderText = "Personel";
+            this.personelDataGridViewTextBoxColumn.Name = "personelDataGridViewTextBoxColumn";
+            // 
+            // fiyatDataGridViewTextBoxColumn
+            // 
+            this.fiyatDataGridViewTextBoxColumn.DataPropertyName = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.HeaderText = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.Name = "fiyatDataGridViewTextBoxColumn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 325);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 359);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Müşteri:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Personel:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Fiyat:";
+            // 
+            // CmbUrun
+            // 
+            this.CmbUrun.FormattingEnabled = true;
+            this.CmbUrun.Location = new System.Drawing.Point(133, 322);
+            this.CmbUrun.Name = "CmbUrun";
+            this.CmbUrun.Size = new System.Drawing.Size(143, 28);
+            this.CmbUrun.TabIndex = 5;
+//          this.CmbUrun.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // CmbMusteri
+            // 
+            this.CmbMusteri.FormattingEnabled = true;
+            this.CmbMusteri.Location = new System.Drawing.Point(133, 359);
+            this.CmbMusteri.Name = "CmbMusteri";
+            this.CmbMusteri.Size = new System.Drawing.Size(143, 28);
+            this.CmbMusteri.TabIndex = 6;
+            // 
+            // CmbPersonel
+            // 
+            this.CmbPersonel.FormattingEnabled = true;
+            this.CmbPersonel.Location = new System.Drawing.Point(133, 394);
+            this.CmbPersonel.Name = "CmbPersonel";
+            this.CmbPersonel.Size = new System.Drawing.Size(143, 28);
+            this.CmbPersonel.TabIndex = 7;
+            // 
+            // TxtFiyat
+            // 
+            this.TxtFiyat.Location = new System.Drawing.Point(133, 428);
+            this.TxtFiyat.Name = "TxtFiyat";
+            this.TxtFiyat.Size = new System.Drawing.Size(143, 26);
+            this.TxtFiyat.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(392, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 40);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtFiyat);
+            this.Controls.Add(this.CmbPersonel);
+            this.Controls.Add(this.CmbMusteri);
+            this.Controls.Add(this.CmbUrun);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.hareketDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projeHareketBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hareketDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.projeHareketBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private HareketDataSet hareketDataSet;
+        private System.Windows.Forms.BindingSource projeHareketBindingSource;
+        private HareketDataSetTableAdapters.ProjeHareketTableAdapter projeHareketTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private HareketDataSet1 hareketDataSet1;
+        private System.Windows.Forms.BindingSource projeHareketBindingSource1;
+        private HareketDataSet1TableAdapters.ProjeHareketTableAdapter projeHareketTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ürünAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn müşteriDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fiyatDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox CmbUrun;
+        private System.Windows.Forms.ComboBox CmbMusteri;
+        private System.Windows.Forms.ComboBox CmbPersonel;
+        private System.Windows.Forms.TextBox TxtFiyat;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HareketProje/HareketProje/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HareketProje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection(@"Data Source=berk;Initial Catalog=Hareket;Integrated Security=True;");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hareketDataSet1.ProjeHareket' table. You can move, or remove it, as needed.
+            this.projeHareketTableAdapter1.Fill(this.hareketDataSet1.ProjeHareket);
+            // TODO: This line of code loads data into the 'hareketDataSet.ProjeHareket' table. You can move, or remove it, as needed.
+            this.projeHareketTableAdapter.Fill(this.hareketDataSet.ProjeHareket);
+
+            // Ürünleri Çekme
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from URUNLER ", baglanti);
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            CmbUrun.ValueMember = "ID";
+            CmbUrun.DisplayMember = "AD";
+            CmbUrun.DataSource = dt;
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+
+            //Müşterileri Çekme
+            baglanti.Open();
+            SqlCommand komut1 = new SqlCommand("select * from MUSTERILER",baglanti);
+            SqlDataAdapter da1 = new SqlDataAdapter(komut1);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            CmbMusteri.ValueMember = "ID";
+            CmbMusteri.DisplayMember = "ADSOYAD";
+            CmbMusteri.DataSource = dt1;
+            komut1.ExecuteNonQuery();
+            baglanti.Close();
+
+            // Perspnelleri Çekme
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("select * from PERSONELLER", baglanti);
+            SqlDataAdapter da2 = new SqlDataAdapter(komut2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            CmbPersonel.ValueMember = "Id";
+            CmbPersonel.DisplayMember = "Ad";
+            CmbPersonel.DataSource = dt2;
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+
+            
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into HAREKETLER(URUN,MUSTERI,PERSONEL,FIYAT)values(@p1,@p2,@p3,@p4)", baglanti);
+            komut.Parameters.AddWithValue("@p1", CmbUrun.Text);
+            komut.Parameters.AddWithValue("@p2", CmbMusteri.Text);
+            komut.Parameters.AddWithValue("@p3", CmbPersonel.Text);
+            komut.Parameters.AddWithValue("@p4", TxtFiyat.Text);
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("İşleminiz Eklendi", "Dikkat", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
